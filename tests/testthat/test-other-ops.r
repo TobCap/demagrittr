a1a5e647035b2eedcac1be06d69740fcd6a1a905
tob <tobakucapitalism@gmail.com>
2016-05-28 14:24:39 +0900

    Add new tests

---
+++ tests/testthat/test-other-ops.r
@@ -0,0 +1,28 @@
+context("other than %>%")
+suppressMessages(library("magrittr"))
+suppressMessages(library("dplyr"))
+
+test_that("equiv value8", {
+  # http://qiita.com/uri/items/17c213bf2bca7c95b154
+  # check above examples
+  e1 <- quote(
+    mtcars %>% lm("mpg ~ wt", data = .) %$% {
+      print(.)
+      #broom::tidy(.) %>% knitr::kable()
+      mean(coefficients) %>% base::abs()
+    }
+  )
+
+  expect_identical(eval(e1), eval(demagrittr(e1)))
+
+  e2 <- quote(
+    iris %>% dplyr::filter(Species == "setosa") %$% {
+      plot(Sepal.Length, Petal.Width)
+      mean(Sepal.Length)
+      res <<- (.)
+    }
+  )
+  expect_identical(eval(e2), eval(demagrittr(e2)))
+
+
+})
