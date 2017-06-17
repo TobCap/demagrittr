648ba41c4df23f7eb9af388afc59808546024f90
example <example@example.com>
2017-06-17 18:00:04 +0900

    Simplify tests

--- tests/testthat/test-dplyr-adhoc.r
+++ tests/testthat/test-dplyr-adhoc.r
@@ -1,11 +1,10 @@
 context("adhoc dplyr")
-suppressMessages(library("magrittr"))
 suppressMessages(library("dplyr"))
 
 test_that("equiv value3", {
-  e1 <- quote(iris %>% filter(Sepal.Width %>% is_greater_than(4.3)))
+  e1 <- quote(iris %>% filter(Sepal.Width %>% `>`(4.3)))
   expect_identical(eval(e1), eval(demagrittr(e1, FALSE)))
 
-  e2 <- quote(filter(iris, Sepal.Width %>% is_greater_than(4.3)))
+  e2 <- quote(filter(iris, Sepal.Width %>% `>`(4.3)))
   expect_identical(eval(e2), eval(demagrittr(e2, FALSE)))
 })
